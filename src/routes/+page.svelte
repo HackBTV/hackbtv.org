5e4c5d6c40ec759337940928ddb9c29fcbb7326e
Cheru Berhanu <cheru@cheru.dev>
2023-02-12 15:58:26 -0500

    aa

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<div class="bg-sky-200 h-screen flex items-center">
+    <div class="flex flex-col items-center w-screen text-gray-800">
+            <h1 class="text-7xl font-semibold mb-10">HackBTV</h1>
+
+        <p class="text-xl m-1">Vermont's largest high school coding event, coming late Spring!</p>
+        <p class="text-xl m-1">Interested? <a href="https://airtable.com/shr9S5rGciACHbyFw" class="underline">Preregister!</a></p>
+        <p class="text-xl m-1">Contact - <a href="mailto:cheru@hackbtv.org" class="underline">cheru@hackbtv.org</a> </p>
+    </div>
+</div>
